9b96f9bde4e39872f6c42e35c400f12a30e523e8
Ardonit Saliji <ardonit.1980@gmail.com>
2022-11-21 10:33:36 +0100

    publish game

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import SnakeBoard from './components/SnakeBoard/SnakeBoard'
+
+import './App.css'
+const App = () => (
+  <div className='App'>
+    <SnakeBoard></SnakeBoard>
+  </div>
+)
+
+export default App
---
+++ src/components/Tile.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import '../index.css'
+const Tile = ({ number }) => {}
+
+export default Tile
